4e9d569914d6413f383a02eabdcd82dd457a26ba
Catherine O'Grady <catherine.ogrady5@mail.dcu.ie>
2017-11-20 23:18:32 +0000

    create user table and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171119135855) do
+ActiveRecord::Schema.define(version: 20171120230408) do
 
   create_table "bicycles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.float    "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
