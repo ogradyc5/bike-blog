42b7f1d3226e446c9949d11402e55c294c920c58
Catherine O'Grady <catherine.ogrady5@mail.dcu.ie>
2017-11-21 18:02:47 +0000

    add secure password to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120234733) do
+ActiveRecord::Schema.define(version: 20171121175822) do
 
   create_table "bicycles", force: :cascade do |t|
     t.string   "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20171120234733) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
