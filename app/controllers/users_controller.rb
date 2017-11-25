f4862440269b3f0ad349a6a98c372d0483eb5850
Catherine O'Grady <catherine.ogrady5@mail.dcu.ie>
2017-11-25 22:05:13 +0000

    add signin upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,8 +24,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success]="Welcome to the CycleHub Club #{@user.username}"
-      redirect_to bicycles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end 
