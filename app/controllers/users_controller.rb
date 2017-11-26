a64d99e36412e8bf8c5748c16a1645c9f195494e
Catherine O'Grady <catherine.ogrady5@mail.dcu.ie>
2017-11-26 18:29:11 +0000

    Changed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,20 +2,9 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
   before_action :require_same_user, only: [:edit, :update, :destroy]
   before_action :require_admin, only: [:destroy]
-  
-  def require_same_user
-    if current_user != @user and !current_user.admin?
-      flash[:danger] = "You can only update your personal account"
-      redirect_to root_path
-    end
-  end
 
   def index 
     @users = User.paginate(page: params[:page], per_page: 5)
-  end 
-  
-  def set_user
-    @user = User.find(params[:id])
   end 
   
   def new
@@ -32,13 +21,6 @@ class UsersController < ApplicationController
       render 'new'
     end 
   end
-  
-  def require_admin
-    if logged_in? && !current_user.admin?
-      flash[:danger] = "Only administrators can perform that action"
-      redirect_to root_path
-    end 
-  end 
   
   def edit
   end 
@@ -67,4 +49,23 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end 
+  
+  def require_same_user
+    if current_user != @user and !current_user.admin?
+      flash[:danger] = "You can only update your personal account"
+      redirect_to root_path
+    end
+  end
+  
+  def require_admin
+    if logged_in? && !current_user.admin?
+      flash[:danger] = "Only administrators can perform that action"
+      redirect_to root_path
+    end 
+  end 
+  
 end
