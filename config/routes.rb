0550cc060868706b3960947c9dffe26b9419e1e2
Catherine O'Grady <catherine.ogrady5@mail.dcu.ie>
2017-11-26 22:09:32 +0000

    add categories model and tests

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,9 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  
+  resources :categories, except: [:destroy]
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171125225124) do
+ActiveRecord::Schema.define(version: 20171126203352) do
 
   create_table "bicycles", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20171125225124) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
