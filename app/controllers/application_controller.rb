6caba6c228e1c52b2282078d17aabd572ffaf1a9
Catherine O'Grady <catherine.ogrady5@mail.dcu.ie>
2017-11-26 17:59:45 +0000

    fixing stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   
   helper_method :current_user, :logged_in?
   
-  def index
-  end
+ # def index
+  #end
   
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
