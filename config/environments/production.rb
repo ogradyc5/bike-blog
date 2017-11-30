f7807aa592496f818f3da37c0ee48eb809544424
Catherine O'Grady <catherine.ogrady5@mail.dcu.ie>
2017-11-30 23:18:29 +0000

    debug uninstall

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.web_console.development_only = true
+  config.web_console.development_only = false
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
